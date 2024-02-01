0f36573913ed1a33456a85c562b1837974767c44
Ashwini202 <ashwinicodetentacles@gmail.com>
2024-02-01 18:44:27 +0530

    ui

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   providerTestNetURL: "https://data-seed-prebsc-1-s1.binance.org:8545/",
   providerMainNetURL: "https://bsc-dataseed1.binance.org",
   providerURL: "https://bsc-dataseed1.binance.org",
-  baseURL: "https://rbits.xyz/boxes/api/",
+  baseURL: "https://rbits.xyz/boxes/backend/api/",
   ownerAddress: "0x66A7B9f608378e59105022aB00b0F541666e8c4d",
   // silverAddress: "0xd27D3F7f329D93d897612E413F207A4dbe8bF799",
   NFTAddress: "0x82A3E038048CF02C19e60856564bE209899d4F12",
