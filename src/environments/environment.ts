d4c1ea0eb12cf86dfb8f14c73ff25b96240764f4
Ashwini202 <ashwinicodetentacles@gmail.com>
2024-03-15 14:19:25 +0530

    ui

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,8 +8,8 @@ export const environment = {
   openseaChainIds: [1, 3, 4,2,5],
   providerURL: "https://data-seed-prebsc-2-s1.bnbchain.org:8545",
   
-  baseURL: "http://codetentacles-006-site31.htempurl.com/api/",
-  // baseURL:'http://moonbox.codetentaclestechnologies.tech/api/api/',
+  // baseURL: "http://codetentacles-006-site31.htempurl.com/api/",
+  baseURL:'http://moonbox.codetentaclestechnologies.tech/api/api/',
   ownerAddress: "0x703632A0b52244fAbca04aaE138fA8EcaF72dCBC",
   // silverAddress: "0x46192Bd44C9066D425375326808109C7d97a2181",
 
@@ -21,8 +21,8 @@ export const environment = {
   tokenContractAddress: "0x3F4E053184Bef016286D07189FB60c61A6eF972F",
   bridgeCollectionAddress:"0x229620efb06cB366350Aae34469792FE1402Dc65",
   configFile: 'testnet',
-  adminPanelUrl : "http://codetentacles-006-site41.htempurl.com/"
-  // adminPanelUrl : 'http://moonbox.codetentaclestechnologies.tech/admin/'
+  // adminPanelUrl : "http://codetentacles-006-site41.htempurl.com/"
+  adminPanelUrl : 'http://moonbox.codetentaclestechnologies.tech/admin/'
 };
 
 /*
