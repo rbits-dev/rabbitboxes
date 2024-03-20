c41468d3e7b32e7393b0cc963b03d57be922b823
rbits <>
2024-03-20 03:10:35 +0100

    change deployment path to /boxes

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/rabbit-boxes'),
+      dir: require('path').join(__dirname, './coverage/boxes'),
       subdir: '.',
       reporters: [
         { type: 'html' },
