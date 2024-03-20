2ee6099927cc28131599fafc341b51a75d230118
rbits <>
2024-03-20 02:53:06 +0100

    add environment definitions for bridge and mint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,12 +9,13 @@ export const environment = {
   ownerAddress: "0x66A7B9f608378e59105022aB00b0F541666e8c4d",
   NFTAddress: "0x82A3E038048CF02C19e60856564bE209899d4F12",
   lootBoxAddress: "0x614A5DD989E5Fc086a0059D37F4EcDeaA6341216", 
-  bridgeCollectionAddress:"0x229620efb06cB366350Aae34469792FE1402Dc65",
+  bridgeCollectionAddress:"",
   artistNFTAddress: "0xE93ddAA2d56F1DAF39177fe875698A147bC94f69",
   tokenContractAddress: "0xa6EbCC4C5C0316191eA95BFC90F591DF23A03DFE",
 
   configFile: 'mainnet',
 
-  adminPanelUrl : "https://rbits.xyz/boxes/admin/"
-
+  adminPanelUrl : "https://rbits.xyz/boxes/admin/",
+  mintNFTAddress:"", //FIXME contract not yet deployed
+  explorerURLForEth:"https://etherscan.io/",
 };
